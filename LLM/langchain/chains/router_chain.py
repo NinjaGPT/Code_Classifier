fee2e8f1354427f86443be990c5daed517f58213
NinjaGPT <hackmine@gmail.com>
2024-03-15 17:01:47 -0400

    Update router_chain.py

--- LLM/langchain/chains/router_chain.py
+++ LLM/langchain/chains/router_chain.py
@@ -80,7 +80,7 @@ prompt_infos = [
 ]
 
 def __init_env__():
-    os.environ['OPENAI_API_KEY'] = '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'
+    os.environ['OPENAI_API_KEY'] = 'sk-XX'
 __init_env__()
 # instance object of ChatGPT
 llm = ChatOpenAI(temperature=0)     
