44a693f90b106d403c7582466a7710b8cb25df3a
NinjaGPT <hackmine@gmail.com>
2024-03-15 17:01:59 -0400

    Update sequential_chain.py

--- LLM/langchain/chains/sequential_chain.py
+++ LLM/langchain/chains/sequential_chain.py
@@ -6,7 +6,7 @@ from langchain.chains import SequentialChain
 import pandas as pd
 
 def __init_env__():
-    os.environ['OPENAI_API_KEY'] = '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'
+    os.environ['OPENAI_API_KEY'] = 'sk-XX'
 
 __init_env__()
 
